9db5226d2f72ea1ebe9121ba5806264b4fc269ab
hadams0 <adamshannah1@icloud.com>
2023-07-06 16:15:44 +0100

    weathercheck conditions added

--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -1,10 +1,17 @@
 public class WeatherCheck {
     public static void main(String[] args) {
 
-        int temperature = 20;
-        boolean currentlyRaining = true;
+        int temperature = 16;
+        boolean currentlyRaining = false;
 
-        
-
+        if (temperature <= 15 && currentlyRaining) {
+            System.out.println("You should wear a raincoat!");
+        } else if (temperature > 15 && currentlyRaining) {
+            System.out.println("You might need an umbrella!");
+        } else if (temperature > 15 && !currentlyRaining) {
+            System.out.println("You should wear sunscreen!");
+        } else if (temperature <= 15 && !currentlyRaining) {
+            System.out.println("You should wear warm clothes!");
+        }
     }
 }
